9dea64c5996025ed2e11863e64b72609b5c3bf1e
kwanik-kor <rhksldr134@naver.com>
2023-01-22 22:58:01 +0900

    Chapter5: 구조체 내용 학습 완료

--- src/main.rs
+++ src/main.rs
@@ -42,3 +42,31 @@ struct Point(i32, i32, i32);
 
 // Struct without fields
 struct AlwaysEqual;
+
+fn calc_area_with_parameter(length: u32, width: u32) -> u32 {
+    length * width
+}
+
+fn calc_area_with_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rctangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
+fn calc_area_with_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
+}
